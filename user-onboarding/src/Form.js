db33a0e8cf3f0cc8e781a9808ec545f78dff43e4
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-15 15:56:36 -0400

    spoken language on browser issue fixed

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -3,17 +3,23 @@ export default function Form(props){
     return(
         <form>
             <div>
-                <label>{'Label '}
-                    <input type='' name='' value='' checked='' onChange=''></input>
+                <label>{'Name'}
+                    <input type='text' name='name' /*value=''*/ checked='' onChange=''/>
                 </label>
 
-                <label>{'Label '}
-                    <input type='' name='' value='' checked='' onChange=''></input>
+                <label>{'Email '}
+                    <input type='text' name='email' /*value=''*/ checked='' onChange=''/>
                 </label>
-                
-                <label>{'Label '}
-                    <input type='' name='' value='' checked='' onChange=''></input>
+
+                <label>{'Password '}
+                    <input type='password' name='password' /*value=''*/ checked='' onChange=''/>
                 </label>
+                <label>{'Terms of Service '}
+                    <input type='checkbox' name='tos' /*checked=''*/ onChange=''/>
+                </label>
+                <div className='submitButton'>
+                <button disabled={true}>submit</button>
+                </div>
             </div>
         </form>
     );
