7466e694f172856fdac2006c0b9a9513d3f068fc
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-15 17:29:09 -0400

    checkbox issue. errors also keep hiding

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,27 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import schema from './schema';
+import * as yup from 'yup';
 
 const initialFormData = {
   name:'',
   email:'',
   password:'',
   passwordConfirmation:'',
-  tos:false,
+  tos: false,
+}
+const initialFormErrors = {
+  name:'',
+  email:'',
+  password:'',
+  passwordConfirmation:'',
+  tos:'',
 }
 const initialDisabled = true;
+
 
 function App() {
   const [formData, setFormData] = useState(initialFormData);
   const [disabled, setDisabled] = useState(initialDisabled);
+  const [errors, setErrors] = useState(initialFormErrors);
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+    .validate(value)
+    .then(()=> setErrors({...FormData, [name]:''}))
+    .catch((err)=> setErrors({...FormData, [name]:err.errors[0]}))
+  }
 
   const inputChange= (name, value)=>{
+    validate(name,value);
     setFormData({
       ...formData, [name]:value
     });
   };
+
+  useEffect(()=>{
+    schema.isValid(formData)
+    .then(valid=> setDisabled(!valid))
+  }, [formData])
 
   return (
     <div className="App">
@@ -29,6 +52,7 @@ function App() {
         values = {formData}
         change = {inputChange}
         disabled = {disabled}
+        errors= {errors}
       />
     </div>
   );
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -5,12 +5,13 @@ export default function Form(props){
         values,
         change,
         disabled,
+        errors
     } = props;
 
     const onChange = evt => {
         const {name, value, checked, type} = evt.target
         const valueToUse = type === 'checkbox' ? checked : value;
-        change(name, value);
+        change(name, valueToUse);
     }
 
     return(
@@ -28,7 +29,7 @@ export default function Form(props){
                     <input type='password' name='password' value={values.password}  onChange={onChange}/>
                 </label>
                 <label>{'Password Confirmation '}
-                    <input type='password' name='password confirmation' value={values.passwordConfirmation}  onChange={onChange}/>
+                    <input type='password' name='passwordConfirmation' value={values.passwordConfirmation}  onChange={onChange}/>
                 </label>
 
                 <label>{'Terms of Service '}
@@ -38,6 +39,14 @@ export default function Form(props){
                 <div className='submitButton'>
                 <button disabled={disabled}>submit</button>
                 </div>
+            </div>
+
+            <div>
+                <div>{errors.name}</div>
+                <div>{errors.email}</div>
+                <div>{errors.password}</div>
+                <div>{errors.passwordConfirmation}</div>
+                <div>{errors.tos}</div>
             </div>
         </form>
     );
