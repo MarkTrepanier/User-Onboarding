9b344244ba2bd83b22549a25bab6ae5fc3638755
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-15 15:27:12 -0400

    initial commit and Form.js early setup

---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+export default function Form(props){
+    return(
+        <form>
+            <div>
+                <label>{'Label '}
+                    <input type='' name='' value='' checked='' onChange=''></input>
+                </label>
+
+                <label>{'Label '}
+                    <input type='' name='' value='' checked='' onChange=''></input>
+                </label>
+                
+                <label>{'Label '}
+                    <input type='' name='' value='' checked='' onChange=''></input>
+                </label>
+            </div>
+        </form>
+    );
+}
