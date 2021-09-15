89e58dffdfc2d6b8993bebc023ac59fd46a35adc
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-15 16:19:52 -0400

    inputs changing as needed

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
+import React, {useState} from 'react';
+
+const initialFormData = {
+  name:'',
+  email:'',
+  password:'',
+  tos:false,
+}
 
 function App() {
+  const [formData, setFormData] = useState(initialFormData)
+  const inputChange= (name, value)=>{
+    setFormData({
+      ...formData, [name]:value
+    })
+  }
+
   return (
     <div className="App">
-      <Form/>
+      <Form
+        values = {formData}
+        change = {inputChange}
+      />
     </div>
   );
 }
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,22 +1,36 @@
 import React from 'react';
+
 export default function Form(props){
+    const {
+        values,
+        change,
+    } = props;
+
+    const onChange = evt => {
+        const {name, value, checked, type} = evt.target
+        const valueToUse = type === 'checkbox' ? checked : value;
+        change(name, value);
+    }
+
     return(
         <form>
             <div>
-                <label>{'Name'}
-                    <input type='text' name='name' /*value=''*/ checked='' onChange=''/>
+                <label>{'Name '}
+                    <input type='text' name='name' value= {values.name} checked='' onChange={onChange}/>
                 </label>
 
                 <label>{'Email '}
-                    <input type='text' name='email' /*value=''*/ checked='' onChange=''/>
+                    <input type='text' name='email' value={values.email} checked='' onChange={onChange}/>
                 </label>
 
                 <label>{'Password '}
-                    <input type='password' name='password' /*value=''*/ checked='' onChange=''/>
+                    <input type='password' name='password' value={values.password} checked='' onChange={onChange}/>
                 </label>
+
                 <label>{'Terms of Service '}
-                    <input type='checkbox' name='tos' /*checked=''*/ onChange=''/>
+                    <input type='checkbox' name='tos' checked={values.tos} onChange={onChange}/>
                 </label>
+
                 <div className='submitButton'>
                 <button disabled={true}>submit</button>
                 </div>
